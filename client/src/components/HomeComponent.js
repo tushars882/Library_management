5ad42884c81e735ace9459b6c2fa67628abe0b4a
tushars882 <tushars882@gmail.com>
2022-05-12 12:24:54 +0530

    added files

---
+++ client/src/components/HomeComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <div className="container mt-4 home text-center align-self-center">
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
